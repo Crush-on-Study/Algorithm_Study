cde224babd7d4b6ccd1b5609296e728453ce6933
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-02-11 12:56:15 +0900

    Create 음료수 얼려 먹기.py

---
+++ 이코테/DFS+BFS/음료수 얼려 먹기.py
@@ -0,0 +1,47 @@
+# DFS 풀이 추가 예정
+
+import sys
+sys.stdin = open("111.txt")
+from collections import deque
+N,M = map(int,input().split()) # N은 세로 M은 가로
+graph = [list(map(int,input())) for _ in range(N)]
+visited = [[False]*M for _ in range(N)]
+
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
+
+cnt = 0
+
+# BFS 풀이
+def bfs_solution():
+    def bfs(y,x):
+        global cnt
+        q = deque()
+        q.append((y,x))
+        
+        while q:
+            y,x= q.popleft()
+            for i in range(4):
+                ny = y+dy[i]
+                nx = x+dx[i]
+                if 0<=ny<N and 0<=nx<M:
+                    if graph[ny][nx] == 0 and not visited[ny][nx]:
+                        visited[ny][nx] = True
+                        cnt+=1
+                        q.append((ny,nx))
+                        
+        return cnt
+
+    arr = []
+
+    for col in range(N):
+        for row in range(M):
+            if graph[col][row] == 0 and not visited[col][row]:    
+                visited[col][row] = True
+                cnt = 1
+                a = bfs(col,row)
+                arr.append(a)
+                
+    print(len(arr))
+
+bfs_solution()
